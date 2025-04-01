4d2757a1ab548c56ec18e69a6727f5a376ddf6d2
ErickMelendez25 <a20213712@pucp.edu.pe>
2025-03-31 21:37:34 -0500

    MEJORANDOlsssjjfmf

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -40,6 +40,27 @@ const Login = () => {
     }
   };
 
+    // Función para obtener la lista de usuarios
+  const fetchUsuarios = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/api/usuarios`);
+    
+      console.log(`${apiUrl}/api/usuarios`);  // Verifica que la URL sea correcta
+      console.log('Usuarios obtenidos:', response.data);  // Muestra los datos obtenidos
+  
+      // Verifica si el contenido que obtienes es HTML
+      if (response.headers['content-type'] && response.headers['content-type'].includes('html')) {
+        console.error('Recibí HTML en lugar de JSON:', response.data);
+        setErrorMessage('Se esperaba un JSON, pero recibí HTML.');
+      } else {
+        setUsuarios(response.data);  // Si la respuesta es JSON, configúralo en el estado
+      }
+    } catch (error) {
+      console.error('Error al obtener usuarios:', error);
+      setErrorMessage('No se pudo obtener la lista de usuarios');
+    }
+  };
+
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleLogin}>
