cc72da88a485fe7b7751d5db0a6126bbed74723e
ErickMelendez25 <a20213712@pucp.edu.pe>
2025-04-02 12:36:16 -0500

    mejorar server 11134566

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();  // Obtener la ruta del directorio actual (cor
 
 // Configura CORS para permitir solicitudes solo desde tu frontend
 const corsOptions = {
-  origin: ['https://sateliterreno-production.up.railway.app', 'http://localhost:5000'],
+  origin: ['https://sateliterrreno-production.up.railway.app', 'http://localhost:5000'],
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 };
