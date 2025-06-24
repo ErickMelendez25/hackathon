eabe9d0ca4c6e1aebd3cb547beb80448001ba2b2
ErickMelendez25 <a20213712@pucp.edu.pe>
2025-06-24 00:10:45 -0500

    subdomnio

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     try {
       // Verifica si estás en producción (Railway) o en desarrollo (localhost)
       const apiUrl = process.env.NODE_ENV === 'production' 
-      ? 'https://hackathon-production-8277.up.railway.app/login' 
+      ? 'https://qiskit.academionlinegpt.com/login' 
       : 'http://localhost:5000/login';
 
       console.log("API URL:", apiUrl);  // Verifica si la URL es correcta
@@ -74,7 +74,7 @@ const Login = () => {
   
       // URL directa al endpoint sin "/api"
       const apiUrl = process.env.NODE_ENV === 'production'
-        ? 'https://hackathon-production-8277.up.railway.app'
+        ? 'https://qiskit.academionlinegpt.com'
         : 'http://localhost:5000';
   
       // Enviar datos al backend
